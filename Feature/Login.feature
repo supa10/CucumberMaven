50a7f80cab66979de7e1e2d2628785166d8d8a51
suyog patil <patil.s.suyog@gmail.com>
2017-10-14 22:50:59 +0530

    Test

---
+++ Feature/Login.feature
@@ -0,0 +1,7 @@
+@run
+Feature: Login Into OHRM website
+
+Scenario: Orange HRMS login and logout 
+Given I login to Orange HRMS
+When I verify homePage
+Then I close browser
---
+++ src/test/java/pageclass/RunnerTest.java
@@ -0,0 +1,14 @@
+package pageclass;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Feature"
+		,glue={"stepdefinition"}, tags="@run"
+		)
+public class RunnerTest {
+
+}
